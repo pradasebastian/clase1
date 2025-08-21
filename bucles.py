52e65a34b85a91df8ed98a39535783c7eea748cb
pradasebastian <227668063+pradasebastian@users.noreply.github.com>
2025-08-21 17:45:12 -0500

    empresario.py

---
+++ bucles.py
@@ -0,0 +1,45 @@
+import os
+os.system("cls")
+
+isActive=True
+
+while isActive:
+    numero=int(input("Introduce un numero: "))
+    if numero == 0:
+       print("El numero es correcto")
+       isActive=False
+    else:
+        print("Ingrese el numero cero")
+
+
+
+
+"""while True:
+    numero=int(input("Introduce un numero:"))
+    if numero == 0:
+      print("El numero es correcto")
+      break
+    else: 
+      print("Ingrese el numero cero")"""
+    
+    
+    
+
+"""palabra=["hola","mundo"]
+
+for i in palabra:
+    print(i)"""
+
+
+
+
+"""palabra=(input("Introduce una palabra:"))
+
+for i in palabra:
+    print(i)"""
+
+
+""""numero=int(input("Introduce un número:"))
+
+for i in range(1, numero+1,2):
+    print(i)"""
